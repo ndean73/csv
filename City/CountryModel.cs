cea87df4400db6e79687d8324bf42ba590ce6b48
ndean73 <Dean.Nalumpa@mlnr.gov.zm>
2020-01-17 05:47:14 -0800

    Feature: Created Separate classes and included Interfaces for CityModel and CountryModel

---
+++ City/CountryModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Cities
+{ 
+    public interface ICountryModel
+   {
+    public IList<T> AddCountries<T>(IEnumerable<T> countryCapitalQuery);
+   }
+    public class CountryModel : ICountryModel
+    {
+
+        public IList<T> AddCountries<T>(IEnumerable<T> countryCapitalQuery)
+        {
+
+            var db = DBConn.conn();
+            return null;
+        }       
+        public string Name { get; set; }
+        public string ISO2 { get; set; }
+        public string ISO3 { get; set; }
+    }
+}
---
+++ City/DBConn.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cities
+{
+    //public interface IDBConn
+    //{
+    //    public CitiesContext conn();
+    
+   // }
+    public class DBConn  ///: IDBConn
+    {
+            public static CitiesContext conn() {
+            var db = new CitiesContext() ;
+            return db;
+        }
+
+    }
+}
